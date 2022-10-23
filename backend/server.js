ca7a6eb1c30a848e623dac3b037a8a76a5cb4037
Blin Kukaj <kblin164@gmail.com>
2022-10-23 18:20:16 +0200

    created a dummy data file and also I have splitted the folder in two folders dedicated to backend and frontend. Installed express for api usage

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Homepage");
+});
+
+
+app.get(\/)
+
+app.listen(3000, () => {
+  console.log("Server started on 3000");
+});
