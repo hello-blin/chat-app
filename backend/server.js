e45e925b8e8bca052597d820c32edad87cc2a0ec
Blin Kukaj <kblin164@gmail.com>
2022-10-23 18:30:05 +0200

    Creating another endpoint for single chat

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 
+const { chats } = require("./data/data");
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Homepage");
 });
 
+//Endpoint for all data of chats
 
-app.get(\/)
+app.get("/api/chat", (req, res) => {
+  res.json(chats);
+});
+
+//Endpoint for single chat
+
+app.get("/api/chat/:id", (req, res) => {
+//   res.send(req.params.chatName);
+
+});
 
 app.listen(3000, () => {
   console.log("Server started on 3000");
